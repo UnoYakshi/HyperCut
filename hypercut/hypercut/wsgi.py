2ddf14402919c8ce4dc3a61fa8137f83e3da4918
UnoYakshi <YellowWanna@gmail.com>
2020-06-05 18:52:28 +0300

    Added Django project and dependencies...

---
+++ hypercut/hypercut/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hypercut project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypercut.settings')
+
+application = get_wsgi_application()
