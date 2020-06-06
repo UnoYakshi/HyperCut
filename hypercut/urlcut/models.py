5c00c67d1a8fefeb679cb202118d0f00385ef3a8
UnoYakshi <YellowWanna@gmail.com>
2020-06-06 06:39:35 +0300

    Added initial model for urlcut...

--- hypercut/urlcut/models.py
+++ hypercut/urlcut/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UrlPair(models.Model):
+    """Full-short URLs pair. Main entity for the entire application..."""
+    full_url = models.TextField()
+    short_url = models.CharField(max_length=50, unique=True, db_index=True)
+
+    usage_count = models.IntegerField(default=0, null=True)
+    usage_count_limit = models.IntegerField(default=-1, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
